8829f67836623a1c8b98e46c4416335f7eabb63c
helenung <uhelen94@gmail.com>
2016-02-27 17:51:04 -0800

    init files

---
+++ catvenger.js
@@ -0,0 +1,43 @@
+$(function() {
+	var neko = $("#neko");
+	neko.mousedown(nekoDown);
+	neko.mouseup(nekoUp);
+	neko.mousemove(nekoMove);
+});
+
+// Called when the mouse button is pressed down on a rectangle.
+// Begins dragging of that rectangle
+function nekoDown(event) {
+	var neko = $("#neko");
+	neko.attr("prevX", event.clientX);
+	neko.attr("prevY", event.clientY);
+	neko.attr("dragging", 1);
+}
+
+// Called when the mouse cursor moves around on a rectangle.
+// If this is done while the mouse button is held down, drags a rectangle.
+function nekoMove(event) {
+	var neko = $(this);
+	var area = $("#area");
+	if(neko.attr("dragging") == 1) {
+		var dy = event.clientY - neko.attr("prevY");
+		var dx = event.clientX - neko.attr("prevX");
+		var oldX = parseInt(neko.position().left);
+		var oldY = parseInt(neko.position().top);
+		neko.css({"top": oldY + dy + "px", "left": oldX + dx + "px"});
+		neko.attr("prevX", event.clientX);
+		neko.attr("prevY", event.clientY);
+		neko.attr("src", "penguin_hang.jpg");
+	} else {
+		neko.attr("src", "penguin.jpg");
+
+	}
+}
+
+// Called when the mouse button is released on a rectangle.
+// Stops any rectangle-dragging action that is in progress.
+function nekoUp() {
+	$("#neko").attr("dragging", 0);
+}
+
+

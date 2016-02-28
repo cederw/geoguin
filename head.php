a6cc9d3009ef1951db9d6e47a58014983740978b
helenung <uhelen94@gmail.com>
2016-02-28 01:40:14 -0800

    sddsf

--- head.php
+++ head.php
@@ -19,5 +19,5 @@
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-	<script src="scripts/jquery.ui.touch-punch.min.js"></script>
+	<script src="jquery.ui.touch-punch.min.js"></script>
 </head>

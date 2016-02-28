9f6f0f7f01ed230d26c166ffab07e6836c7fcd0e
unknown <cederw@uw.edu>
2016-02-27 19:38:31 -0800

    added service

--- catService.php
+++ catService.php
@@ -3,7 +3,7 @@
 	$userID = $_GET['userid'];
 	$lat = $_GET['lat'];
 	$lon = $_GET['lon'];
-
+/*
 	$dbh = new PDO('mysql:host=localhost;port=3306;dbname=cederw_cats', 'cederw_cats', 'cats2!', array( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION));
 
     $stmt = $dbh->prepare("SELECT c.name, c.desc, c.url FROM cat c JOIN user u ON c.userID = u.id WHERE u.id = ".$userID);
@@ -24,4 +24,6 @@
 	//sql all cats who have visted the user, check if any have timed out, and remove them
 	//sql all cats who live near the coords
 	//return an array of the cats who meet the params
+
+	*/
 ?>
